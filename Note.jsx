16370209fafb63cd0e952554f93b6f2eaf182654
tamana007 <tamana.efatwira1@gmail.com>
2023-11-23 14:24:28 -0800

    states created

--- Note.jsx
+++ Note.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Notes.scss'
 
 function Note () {
+
+  
   return (
-    <article> every single note goes here</article>
+    <div className='note-container'>
+    <div className='text'>
+      <div className='textare-container'>
+          <textarea className='textarea'
+            // value={editedNote}
+            // onChange={}
+          />
+          <button className='delete-btn' type='btn'>delet</button>
+          {/* <button>Save</button> */}
+          </div>
+        </div>
+        </div>  
+    
   )
 }
 
--- Notes.jsx
+++ Notes.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './Note'
+import './Notes.scss'
+
 
 function Notes() {
+ const [isClick,setIsclick]=useState(false);
+const [note,setNote]=useState([]);
+
+function handleBtn(){
+  setIsclick(!isClick);
+  if (!isClick){
+    setNote((prev)=>[...note,{id:prev.length+1,Text:'new note'}])
+    console.log(note);
+  }
+}
+
   return (
-    <div>
-      <h4>Notes</h4>
-      <button>Create Notes</button>
-      {/* <Note/> */}
+    <div className='Notes'> 
+    <div className='items'>
+     <h4 >Notes</h4>
+      <button onClick={handleBtn}  className='note-btn'>Create Notes</button>
+      <div className='notescom'>
+        {note.map((item)=>{return <Note key={item.id}/>})}
+      
+      {/* <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/>
+      <Note/> */}
+      </div>
+    </div>
     </div>
   )
 }
