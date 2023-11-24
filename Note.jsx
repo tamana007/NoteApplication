a11b58f9e76a841d2f3349ec8f6cf129f72219ea
tamana007 <tamana.efatwira1@gmail.com>
2023-11-24 15:58:51 -0800

    saved

--- Note.jsx
+++ Note.jsx
@@ -1,24 +1,71 @@
-import React from 'react'
-import './Notes.scss'
+import React, { useState } from "react";
+import "./Notes.scss";
+import { MdDeleteSweep } from "react-icons/md";
 
-function Note () {
+function Note({ note, setNote, id, isSave, setSave }) {
+  console.log("my note here", note);
 
-  
+  const [text, setText] = useState("");
+
+  //::::::::::::SAVE Function:::::::::::::::::::::::::
+  function handleSave(p) {
+    if (text) {
+      // setNote((v) => [...note, { id: v.length + 1, Text: text }]);
+      setSave(!isSave);
+      console.log("save from set", isSave);
+    }
+    // console.log('note in save',note);
+    // console.log('txt here',text);
+  }
+  //::::::::::::::::::::::DELETE Function::::::::::::::::
+
+  function handleDelete() {
+    // const updateNote=note.filter((item)=>item.id !==id)
+    // console.log('handled delete');
+    // setNote(updateNote)
+
+    const newNote = note.filter((note) => note.id !== id);
+    setNote(newNote);
+    // console.log("newNote", newNote);
+    // console.log("from delete", note);
+  }
   return (
-    <div className='note-container'>
-    <div className='text'>
-      <div className='textare-container'>
-          <textarea className='textarea'
-            // value={editedNote}
-            // onChange={}
-          />
-          <button className='delete-btn' type='btn'>delet</button>
-          {/* <button>Save</button> */}
+    <div className="note-container">
+      {/* <div className={`note-container ${isSave ? "saved" : "note-container"}`}> */}
+ <div className="text">
+        <div className={`mini-container ${isSave?"saved":""}`}>
+          
+          {/* It is only white area container */}
+          <div
+            className={`textare-container`}
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          >
+            <textarea className="textarea" />
+            <button onClick={() => handleDelete()} type="btn">
+              <MdDeleteSweep className="handle-btn" />
+            </button>
+            <div className="handle-btn">
+              <button onClick={handleSave} className="save-btn" type="btn">
+                save
+              </button>
+              <button
+                onClick={() => handleDelete()}
+                className="delete-btn"
+                type="btn"
+              >
+                <MdDeleteSweep />
+              </button>
+            </div>
+
+            {/* <button>Save</button> */}
           </div>
         </div>
-        </div>  
-    
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Note
+export default Note;
--- Notes.jsx
+++ Notes.jsx
@@ -1,46 +1,57 @@
-import React, { useState } from 'react'
-import Note from './Note'
-import './Notes.scss'
-
+import React, { useState } from "react";
+import Note from "./Note";
+import "./Notes.scss";
+import { GrNotes } from "react-icons/gr";
+import { LuPencilLine } from "react-icons/lu";
+// import { MdDeleteSweep } from "react-icons/md";
+// import {<FontAwesomeIcon icon="fa-solid fa-notes" />} from 'react-icon/fa'
 
 function Notes() {
- const [isClick,setIsclick]=useState(false);
-const [note,setNote]=useState([]);
+  const [isClick, setIsclick] = useState(false);
+  const [note, setNote] = useState([]);
+  const [isSaveClick, setIsSaveClick] = useState(false);
 
-function handleBtn(){
-  setIsclick(!isClick);
-  if (!isClick){
-    setNote((prev)=>[...note,{id:prev.length+1,Text:'new note'}])
-    console.log(note);
+  function handleBtn() {
+    setIsclick(!isClick);
+    if (!isClick) {
+      setNote((prev) => [...note, { id: prev.length + 1, Text: "new note" }]);
+      console.log(note);
+    }
   }
-}
 
   return (
-    <div className='Notes'> 
-    <div className='items'>
-     <h4 >Notes</h4>
-      <button onClick={handleBtn}  className='note-btn'>Create Notes</button>
-      <div className='notescom'>
-        {note.map((item)=>{return <Note key={item.id}/>})}
-      
-      {/* <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-      <Note/> */}
+    <div className="Notes">
+      <div className="items">
+        <div>
+          <h4>
+            Notes
+            <GrNotes className="note-icon" />
+          </h4>
+        </div>
+
+        <div className="btn-container">
+          <button onClick={handleBtn} className="note-btn">
+            <LuPencilLine className="pen-icon" />
+            Create Notes
+          </button>
+        </div>
+        <div className="notescomp">
+          {note.map((item) => {
+            return (
+              <Note
+                key={item.id}
+                note={note}
+                setNote={setNote}
+                id={item.id}
+                isSave={isSaveClick}
+                setSave={setIsSaveClick}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Notes
+export default Notes;
